4c30dc9efbfc3dc7675a49282d9c8fd743c864d9
Gizmo-Verindipencil <gizmo.verindipencil.19920804@gmail.com>
2021-03-19 22:09:19 +0900

    Fix #12

--- SourceCode/FileScout/ScoutingMethods/RowTextLengthStandardDeviationScoutingMethod.cs
+++ SourceCode/FileScout/ScoutingMethods/RowTextLengthStandardDeviationScoutingMethod.cs
@@ -1,13 +1,14 @@
 ﻿using FileScout.Interfaces;
+using System;
 using System.IO;
 using System.Linq;
 
 namespace FileScout.ScoutingMethods
 {
     /// <summary>
-    /// 行文字数の平均の調査手段を提供します。
+    /// 行文字数の標準偏差の調査手段を提供します。
     /// </summary>
-    public class RowTextLengthAverageScoutingMethod : IScoutingMethod
+    public class RowTextLengthStandardDeviationScoutingMethod : IScoutingMethod
     {
         /// <inheritdoc/>
         public string Do(IScoutingClue clue)
@@ -17,9 +18,22 @@ namespace FileScout.ScoutingMethods
 
             var text = File.ReadLines(clue.FilePath, clue.Encoding);
             if (text.Count() == 0) return "0";
-            return text
+
+            // 文字数の平均を算出
+            var average = text
                 .Select(x => x.Length)
-                .Average().ToString();
+                .Average();
+
+            // 各行の文字数 - 平均文字数の二乗を合計
+            var varianceSum = text
+                .Select(x => Math.Pow(x.Length - average, 2))
+                .Sum();
+
+            // 分散を算出
+            var varianceAverage = varianceSum / text.Count();
+
+            // 分散の平方根を返す
+            return Math.Sqrt(varianceAverage).ToString();
         }
     }
 }
